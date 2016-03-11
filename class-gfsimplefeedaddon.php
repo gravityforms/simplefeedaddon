6462d269e09b3fce865b4ac24ed18544b9967ab1
richardW8k <richard@wawrzyniak.me>
2016-03-11 17:32:23 +0000

    Split into two files, coding standards and docblocks

---
+++ class-gfsimplefeedaddon.php
@@ -0,0 +1,294 @@
+<?php
+
+GFForms::include_feed_addon_framework();
+
+class GFSimpleFeedAddOn extends GFFeedAddOn {
+
+	protected $_version = GF_SIMPLE_FEED_ADDON_VERSION;
+	protected $_min_gravityforms_version = '1.9.16';
+	protected $_slug = 'simplefeedaddon';
+	protected $_path = 'simplefeedaddon/simplefeedaddon.php';
+	protected $_full_path = __FILE__;
+	protected $_title = 'Gravity Forms Simple Feed Add-On';
+	protected $_short_title = 'Simple Feed Add-On';
+
+	private static $_instance = null;
+
+	/**
+	 * Get an instance of this class.
+	 *
+	 * @return GFSimpleFeedAddOn
+	 */
+	public static function get_instance() {
+		if ( self::$_instance == null ) {
+			self::$_instance = new GFSimpleFeedAddOn();
+		}
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Plugin starting point. Handles hooks, loading of language files and PayPal delayed payment support.
+	 */
+	public function init() {
+
+		parent::init();
+
+		$this->add_delayed_payment_support(
+			array(
+				'option_label' => esc_html__( 'Subscribe contact to service x only when payment is received.', 'simplefeedaddon' )
+			)
+		);
+
+	}
+
+
+	// # FEED PROCESSING -----------------------------------------------------------------------------------------------
+
+	/**
+	 * Process the feed e.g. subscribe the user to a list.
+	 *
+	 * @param array $feed The feed object to be processed.
+	 * @param array $entry The entry object currently being processed.
+	 * @param array $form The form object currently being processed.
+	 *
+	 * @return bool|void
+	 */
+	public function process_feed( $feed, $entry, $form ) {
+		$feedName  = $feed['meta']['feedName'];
+		$mytextbox = $feed['meta']['mytextbox'];
+		$checkbox  = $feed['meta']['mycheckbox'];
+
+		// Retrieve the name => value pairs for all fields mapped in the 'mappedFields' field map.
+		$field_map = $this->get_field_map_fields( $feed, 'mappedFields' );
+
+		// Loop through the fields from the field map setting building an array of values to be passed to the third-party service.
+		$merge_vars = array();
+		foreach ( $field_map as $name => $field_id ) {
+
+			// Get the field value for the specified field id
+			$merge_vars[ $name ] = $this->get_field_value( $form, $entry, $field_id );
+
+		}
+
+		// Send the values to the third-party service.
+	}
+
+	/**
+	 * Custom format the phone type field values before they are returned by $this->get_field_value().
+	 *
+	 * @param array $entry The Entry currently being processed.
+	 * @param string $field_id The ID of the Field currently being processed.
+	 * @param GF_Field_Phone $field The Field currently being processed.
+	 *
+	 * @return string
+	 */
+	public function get_phone_field_value( $entry, $field_id, $field ) {
+
+		// Get the field value from the Entry Object.
+		$field_value = rgar( $entry, $field_id );
+
+		// If there is a value and the field phoneFormat setting is set to standard reformat the value.
+		if ( ! empty( $field_value ) && $field->phoneFormat == 'standard' && preg_match( '/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/', $field_value, $matches ) ) {
+			$field_value = sprintf( '%s-%s-%s', $matches[1], $matches[2], $matches[3] );
+		}
+
+		return $field_value;
+	}
+
+	// # SCRIPTS & STYLES -----------------------------------------------------------------------------------------------
+
+	/**
+	 * Return the scripts which should be enqueued.
+	 *
+	 * @return array
+	 */
+	public function scripts() {
+		$scripts = array(
+			array(
+				'handle'  => 'my_script_js',
+				'src'     => $this->get_base_url() . '/js/my_script.js',
+				'version' => $this->_version,
+				'deps'    => array( 'jquery' ),
+				'strings' => array(
+					'first'  => esc_html__( 'First Choice', 'simplefeedaddon' ),
+					'second' => esc_html__( 'Second Choice', 'simplefeedaddon' ),
+					'third'  => esc_html__( 'Third Choice', 'simplefeedaddon' ),
+				),
+				'enqueue' => array(
+					array(
+						'admin_page' => array( 'form_settings' ),
+						'tab'        => 'simplefeedaddon',
+					),
+				),
+			),
+		);
+
+		return array_merge( parent::scripts(), $scripts );
+	}
+
+	/**
+	 * Return the stylesheets which should be enqueued.
+	 *
+	 * @return array
+	 */
+	public function styles() {
+
+		$styles = array(
+			array(
+				'handle'  => 'my_styles_css',
+				'src'     => $this->get_base_url() . '/css/my_styles.css',
+				'version' => $this->_version,
+				'enqueue' => array(
+					array( 'field_types' => array( 'poll' ) ),
+				),
+			),
+		);
+
+		return array_merge( parent::styles(), $styles );
+	}
+
+	// # ADMIN FUNCTIONS -----------------------------------------------------------------------------------------------
+
+	/**
+	 * Creates a custom page for this add-on.
+	 */
+	public function plugin_page() {
+		echo 'This page appears in the Forms menu';
+	}
+
+	/**
+	 * Configures the settings which should be rendered on the add-on settings tab.
+	 *
+	 * @return array
+	 */
+	public function plugin_settings_fields() {
+		return array(
+			array(
+				'title'  => esc_html__( 'Simple Add-On Settings', 'simplefeedaddon' ),
+				'fields' => array(
+					array(
+						'name'    => 'textbox',
+						'tooltip' => esc_html__( 'This is the tooltip', 'simplefeedaddon' ),
+						'label'   => esc_html__( 'This is the label', 'simplefeedaddon' ),
+						'type'    => 'text',
+						'class'   => 'small',
+					),
+				),
+			),
+		);
+	}
+
+	/**
+	 * Configures the settings which should be rendered on the feed edit page in the Form Settings > Simple Feed Add-On area.
+	 *
+	 * @return array
+	 */
+	public function feed_settings_fields() {
+		return array(
+			array(
+				'title'  => esc_html__( 'Simple Feed Settings', 'simplefeedaddon' ),
+				'fields' => array(
+					array(
+						'label'   => esc_html__( 'Feed name', 'simplefeedaddon' ),
+						'type'    => 'text',
+						'name'    => 'feedName',
+						'tooltip' => esc_html__( 'This is the tooltip', 'simplefeedaddon' ),
+						'class'   => 'small',
+					),
+					array(
+						'label'   => esc_html__( 'Textbox', 'simplefeedaddon' ),
+						'type'    => 'text',
+						'name'    => 'mytextbox',
+						'tooltip' => esc_html__( 'This is the tooltip', 'simplefeedaddon' ),
+						'class'   => 'small',
+					),
+					array(
+						'label'   => esc_html__( 'My checkbox', 'simplefeedaddon' ),
+						'type'    => 'checkbox',
+						'name'    => 'mycheckbox',
+						'tooltip' => esc_html__( 'This is the tooltip', 'simplefeedaddon' ),
+						'choices' => array(
+							array(
+								'label' => esc_html__( 'Enabled', 'simplefeedaddon' ),
+								'name'  => 'mycheckbox',
+							),
+						),
+					),
+					array(
+						'name'      => 'mappedFields',
+						'label'     => esc_html__( 'Map Fields', 'simplefeedaddon' ),
+						'type'      => 'field_map',
+						'field_map' => array(
+							array(
+								'name'       => 'email',
+								'label'      => esc_html__( 'Email', 'simplefeedaddon' ),
+								'required'   => 0,
+								'field_type' => array( 'email', 'hidden' ),
+								'tooltip' => esc_html__( 'This is the tooltip', 'simplefeedaddon' ),
+							),
+							array(
+								'name'     => 'name',
+								'label'    => esc_html__( 'Name', 'simplefeedaddon' ),
+								'required' => 0,
+							),
+							array(
+								'name'       => 'phone',
+								'label'      => esc_html__( 'Phone', 'simplefeedaddon' ),
+								'required'   => 0,
+								'field_type' => 'phone',
+							),
+						),
+					),
+					array(
+						'name'           => 'condition',
+						'label'          => esc_html__( 'Condition', 'simplefeedaddon' ),
+						'type'           => 'feed_condition',
+						'checkbox_label' => esc_html__( 'Enable Condition', 'simplefeedaddon' ),
+						'instructions'   => esc_html__( 'Process this simple feed if', 'simplefeedaddon' ),
+					),
+				),
+			),
+		);
+	}
+
+	/**
+	 * Configures which columns should be displayed on the feed list page.
+	 *
+	 * @return array
+	 */
+	public function feed_list_columns() {
+		return array(
+			'feedName'  => esc_html__( 'Name', 'simplefeedaddon' ),
+			'mytextbox' => esc_html__( 'My Textbox', 'simplefeedaddon' ),
+		);
+	}
+
+	/**
+	 * Format the value to be displayed in the mytextbox column.
+	 *
+	 * @param array $feed The feed being included in the feed list.
+	 *
+	 * @return string
+	 */
+	public function get_column_value_mytextbox( $feed ) {
+		return '<b>' . rgars( $feed, 'meta/mytextbox' ) . '</b>';
+	}
+
+	/**
+	 * Prevent feeds being listed or created if an api key isn't valid.
+	 *
+	 * @return bool
+	 */
+	public function can_create_feed() {
+
+		// Get the plugin settings.
+		$settings = $this->get_plugin_settings();
+
+		// Access a specific setting e.g. an api key
+		$key = rgar( $settings, 'apiKey' );
+
+		return true;
+	}
+
+}
--- simplefeedaddon.php
+++ simplefeedaddon.php
@@ -3,12 +3,12 @@
 Plugin Name: Gravity Forms Simple Feed Add-On
 Plugin URI: http://www.gravityforms.com
 Description: A simple add-on to demonstrate the use of the Add-On Framework
-Version: 1.0
+Version: 2.0
 Author: Rocketgenius
 Author URI: http://www.rocketgenius.com
 
 ------------------------------------------------------------------------
-Copyright 2012-2013 Rocketgenius Inc.
+Copyright 2012-2016 Rocketgenius Inc.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -25,157 +25,25 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
+define( 'GF_SIMPLE_FEED_ADDON_VERSION', '2.0' );
 
-//------------------------------------------
-if (class_exists("GFForms")) {
-    GFForms::include_feed_addon_framework();
+add_action( 'gform_loaded', array( 'GF_Simple_Feed_AddOn_Bootstrap', 'load' ), 5 );
 
-    class GFSimpleFeedAddOn extends GFFeedAddOn {
+class GF_Simple_Feed_AddOn_Bootstrap {
 
-        protected $_version = "1.0";
-        protected $_min_gravityforms_version = "1.7.9999";
-        protected $_slug = "simplefeedaddon";
-        protected $_path = "simplefeedaddon/simplefeedaddon.php";
-        protected $_full_path = __FILE__;
-        protected $_title = "Gravity Forms Simple Feed Add-On";
-        protected $_short_title = "Simple Feed Add-On";
+	public static function load() {
 
-        public function plugin_page() {
-            ?>
-            This page appears in the Forms menu
-        <?php
-        }
+		if ( ! method_exists( 'GFForms', 'include_feed_addon_framework' ) ) {
+			return;
+		}
 
-        public function feed_settings_fields() {
-            return array(
-                array(
-                    "title"  => "Simple Feed Settings",
-                    "fields" => array(
-                        array(
-                            "label"   => "Feed name",
-                            "type"    => "text",
-                            "name"    => "feedName",
-                            "tooltip" => "This is the tooltip",
-                            "class"   => "small"
-                        ),
-                        array(
-                            "label"   => "Textbox",
-                            "type"    => "text",
-                            "name"    => "mytextbox",
-                            "tooltip" => "This is the tooltip",
-                            "class"   => "small"
-                        ),
-                        array(
-                            "label"   => "My checkbox",
-                            "type"    => "checkbox",
-                            "name"    => "mycheckbox",
-                            "tooltip" => "This is the tooltip",
-                            "choices" => array(
-                                array(
-                                    "label" => "Enabled",
-                                    "name"  => "mycheckbox"
-                                )
-                            )
-                        ),
-                        array(
-                            "name" => "mappedFields",
-                            "label" => "Map Fields",
-                            "type" => "field_map",
-                            "field_map" => array(   array("name" => "email", "label" => "Email", "required" => 0),
-                                                    array("name" => "name", "label" => "Name", "required" => 0)
-                            )
-                        ),
-                        array(
-                            "name" => "condition",
-                            "label" => __("Condition", "simplefeedaddon"),
-                            "type" => "feed_condition",
-                            "checkbox_label" => __('Enable Condition', 'simplefeedaddon'),
-                            "instructions" => __("Process this simple feed if", "simplefeedaddon")
-                        ),
-                    )
-                )
-            );
-        }
+		require_once( 'class-gfsimplefeedaddon.php' );
 
-        public function feed_list_columns() {
-            return array(
-                'feedName' => __('Name', 'simplefeedaddon'),
-                'mytextbox' => __('My Textbox', 'simplefeedaddon')
-            );
-        }
+		GFAddOn::register( 'GFSimpleFeedAddOn' );
+	}
 
-        // customize the value of mytextbox before it's rendered to the list
-        public function get_column_value_mytextbox($feed){
-            return "<b>" . $feed["meta"]["mytextbox"] ."</b>";
-        }
+}
 
-        public function plugin_settings_fields() {
-            return array(
-                array(
-                    "title"  => "Simple Add-On Settings",
-                    "fields" => array(
-                        array(
-                            "name"    => "textbox",
-                            "tooltip" => "This is the tooltip",
-                            "label"   => "This is the label",
-                            "type"    => "text",
-                            "class"   => "small"
-                        )
-                    )
-                )
-            );
-        }
-
-        public function scripts() {
-            $scripts = array(
-                array("handle"  => "my_script_js",
-                      "src"     => $this->get_base_url() . "/js/my_script.js",
-                      "version" => $this->_version,
-                      "deps"    => array("jquery"),
-                      "strings" => array(
-                          'first'  => __("First Choice", "simplefeedaddon"),
-                          'second' => __("Second Choice", "simplefeedaddon"),
-                          'third'  => __("Third Choice", "simplefeedaddon")
-                      ),
-                      "enqueue" => array(
-                          array(
-                              "admin_page" => array("form_settings"),
-                              "tab"        => "simplefeedaddon"
-                          )
-                      )
-                ),
-
-            );
-
-            return array_merge(parent::scripts(), $scripts);
-        }
-
-        public function styles() {
-
-            $styles = array(
-                array("handle"  => "my_styles_css",
-                      "src"     => $this->get_base_url() . "/css/my_styles.css",
-                      "version" => $this->_version,
-                      "enqueue" => array(
-                          array("field_types" => array("poll"))
-                      )
-                )
-            );
-
-            return array_merge(parent::styles(), $styles);
-        }
-
-        public function process_feed($feed, $entry, $form){
-            $feedName = $feed["meta"]["feedName"];
-            $mytextbox = $feed["meta"]["mytextbox"];
-            $checkbox = $feed["meta"]["mycheckbox"];
-            $mapped_email = $feed["meta"]["mappedFields_email"];
-            $mapped_name = $feed["meta"]["mappedFields_name"];
-
-            $email = $entry[$mapped_email];
-            $name = $entry[$mapped_name];
-        }
-    }
-
-    new GFSimpleFeedAddOn();
+function gf_simple_feed_addon() {
+	return GFSimpleFeedAddOn::get_instance();
 }
